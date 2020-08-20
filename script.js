591f3c806a4199cce1450c75f1c167d1e836d5d1
Paige Burlew <paigeburlew@gmail.com>
2020-08-20 14:32:37 -0500

    changing concat to +=

--- script.js
+++ script.js
@@ -5,23 +5,31 @@
 var generateBtn = document.querySelector("#generate");
 //Arrays of characters 
 var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = "0123456789";
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var totalCharacters = [];
 
 //confirmation variables for usage of different characters
 
+function writePassword() {
+  // var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+    passwordText.value =generatePassword();
 
+}
 //return a string
 function generatePassword() {
-   
-
-
-   function confirmCharacters() {
+  var userPassword = "";
+  selectedCharacters = "";
+  //  function confirmCharacters() {
       // get user options and store in vars
-      prompt('how many characters would you like your password to be?');
-      // check for length (it should be longer than 8 and shorter than 128)
+    var  confirmLength = prompt('how many characters would you like your password to be?');
+
+     
+        if (confirmLength >= 8) {
+             // check for length (it should be longer than 8 and shorter than 128)
     var confirmLower = confirm("do you want lowercase numbers?");
     var confirmUpper = confirm("do you want uppercase numbers?");
     var confirmNumeric = confirm("do you want numeric?");
@@ -33,41 +41,37 @@ function generatePassword() {
       // check that at least one is true
         // else - alert user
         if (confirmLower) {
-          alert ("add lowercase letters.");
-          selectedCharacters.concat (lowerCasedCharacters)
+          selectedCharacters.concat(lowerCasedCharacters)
+          console.log(selectedCharacters);
         }
         if (confirmUpper) {
-          alert ("add uppercase letters.");
-          selectedCharacters.concat (upperCasedCharacters)
+          selectedCharacters.concat(upperCasedCharacters)
         }
         if (confirmNumeric) {
-          alert ("add numeric characters.");
-          selectedCharacters.concat (numericCharacters)
+          selectedCharacters += numericCharacters
         }
         if (confirmspecialCharacter) {
-          alert ("add special characters.");
-          selectedCharacters.concat (specialCharacters)
+          selectedCharacters.concat(specialCharacters)
         }
         if (confirmLower === false && !confirmUpper) {
           alert ("please pick at least one character type");
-          getUserOptions;
           return;
         }
-        writePassword();
+        for (i = 0; i < confirmLength; i++ ){
+          userPassword += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
+
+        }
+        console.log(userPassword);
+          return userPassword;
+        }
+        else {
+          alert ("please choose a length between 8 and 128")
+        };
+      
     }
-};
+// };
 
-    function writePassword(confirmCharacters) {
-       var passwordText = document.querySelector("#password");
-        passwordText.value = password;
-      var password = "";
-
-      for (var i =0; i < promptLength; i++){
-
-        return password;
-      }
-
-    }
+    
 
       // if lower
         // push a random lower char to password
@@ -91,8 +95,7 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 
-
+// TO DO- change all arrays to strings and change concat to +=
 
 
 
